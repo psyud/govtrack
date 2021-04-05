c848a59ec8f7a9f9111a7efd16475df9a30ced2e
X <psyud@pm.me>
2021-04-05 17:39:30 +0700

    Add spinner for a less dummy UX

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import { Provider } from 'react-redux'
+import NextNprogress from 'nextjs-progressbar';
 import  store from '../store'
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
+      <NextNprogress options={{ showSpinner: false }}/>
       <Component {...pageProps} />
     </Provider>
   )
