b32e39a0f250f6343e579361f38da4982d40193f
Renegade <psyud@pm.me>
2021-04-07 10:46:24 +0700

    Create a project

--- pages/grants/[id]/apply.tsx
+++ pages/grants/[id]/apply.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import React, { Component } from "react";
 import { Button, Checkbox, Dropdown, Form, Grid, Message, Table } from "semantic-ui-react";
-import { IProps } from ".";
 import Layout from "../../../components/Layout";
+import { getReadonlyContract as getServerContract } from "../../../ethereum/serverContract";
+import GrantOpportunity from "../../../models/GrantOppurtunity";
 
 const projects = [
     {
@@ -12,77 +13,84 @@ const projects = [
     }
 ];
 
-export default class Apply extends Component<IProps> {
-    static async getInitialProps(props: any) {
-        const { id } = props.query;
-        if(typeof id === 'undefined'){
-            return {};
-        }
+interface IProps{
+    grant: GrantOpportunity
+}
 
-        return {
-            id
-        }
+export default function Apply(props: IProps){
+    return <Layout>
+        <Grid columns={3}>
+            <Grid.Column width={4}></Grid.Column>
+            <Grid.Column width={8}>
+                <Form>
+                    <Form.Field>
+                        <label>Project</label>
+                        <Dropdown 
+                            placeholder='Select Project'
+                            fluid
+                            selection
+                            options={projects}
+                        />
+                    </Form.Field>
+                    <Message>Don't have any project? Click <Link href="/projects/new">here</Link> to create one.</Message>
+                    <Table fixed>
+                        <Table.Body>
+                            <Table.Row>
+                                <Table.Cell textAlign='right'><b>Project Name</b></Table.Cell>
+                                <Table.Cell>Project Catalyst</Table.Cell>
+                            </Table.Row>
+                            <Table.Row>
+                                <Table.Cell textAlign='right'><b>Description</b></Table.Cell>
+                                <Table.Cell>Lorem ipsum dolor sit amet, consectetur adipiscing elit</Table.Cell>
+                            </Table.Row>
+                            </Table.Body>
+                    </Table>
+                    <div style={{ textAlign: 'center'}}>
+                        <Button disabled>Applying to</Button>
+                    </div>
+                    <Table fixed>
+                        <Table.Body>
+                            <Table.Row>
+                                    <Table.Cell textAlign='right'><b>Funding Opportunity Number</b></Table.Cell>
+                                    <Table.Cell>{props.grant.id}</Table.Cell>
+                            </Table.Row>
+                            <Table.Row>
+                                    <Table.Cell textAlign='right'><b>Funding Opportunity Title</b></Table.Cell>
+                                    <Table.Cell>{props.grant.title}</Table.Cell>
+                            </Table.Row>
+                            <Table.Row>
+                                    <Table.Cell textAlign='right'><b>Agency</b></Table.Cell>
+                                    <Table.Cell>{props.grant.agencyName}</Table.Cell>
+                            </Table.Row>
+                            
+                        </Table.Body>
+                    </Table>
+                    <Form.Field>
+                        <Checkbox label='I agree to the Terms and Conditions' />
+                    </Form.Field>
+                    <div style={{ textAlign: 'center' }}>
+                        <Button  type='submit'>Submit</Button>
+                    </div>
+                </Form>
+            </Grid.Column>
+            <Grid.Column width={4}></Grid.Column>
+        </Grid>
+        
+    </Layout>
+}
+
+Apply.getInitialProps = async (props: any) => {
+    const { id } = props.query;
+    if(typeof id === 'undefined'){
+        return {};
     }
 
-    render() {
-        return <Layout>
-            <Grid columns={3}>
-                <Grid.Column width={4}></Grid.Column>
-                <Grid.Column width={8}>
-                    <Form>
-                        <Form.Field>
-                            <label>Project</label>
-                            <Dropdown 
-                                placeholder='Select Project'
-                                fluid
-                                selection
-                                options={projects}
-                            />
-                        </Form.Field>
-                        <Message>Don't have any project? Click <Link href="/">here</Link> to create one.</Message>
-                        <Table fixed>
-                            <Table.Body>
-                                <Table.Row>
-                                    <Table.Cell textAlign='right'><b>Project Name</b></Table.Cell>
-                                    <Table.Cell>Project Catalyst</Table.Cell>
-                                </Table.Row>
-                                <Table.Row>
-                                    <Table.Cell textAlign='right'><b>Description</b></Table.Cell>
-                                    <Table.Cell>Lorem ipsum dolor sit amet, consectetur adipiscing elit</Table.Cell>
-                                </Table.Row>
-                                </Table.Body>
-                        </Table>
-                        <div style={{ textAlign: 'center'}}>
-                            <Button disabled>Applying to</Button>
-                        </div>
-                        <Table fixed>
-                            <Table.Body>
-                                <Table.Row>
-                                        <Table.Cell textAlign='right'><b>Funding Opportunity Number</b></Table.Cell>
-                                        <Table.Cell>972350932</Table.Cell>
-                                </Table.Row>
-                                <Table.Row>
-                                        <Table.Cell textAlign='right'><b>Funding Opportunity Title</b></Table.Cell>
-                                        <Table.Cell>Lorem Ipsum</Table.Cell>
-                                </Table.Row>
-                                <Table.Row>
-                                        <Table.Cell textAlign='right'><b>Agency</b></Table.Cell>
-                                        <Table.Cell>FEMA</Table.Cell>
-                                </Table.Row>
-                                
-                            </Table.Body>
-                        </Table>
-                        <Form.Field>
-                            <Checkbox label='I agree to the Terms and Conditions' />
-                        </Form.Field>
-                        <div style={{ textAlign: 'center' }}>
-                            <Button  type='submit'>Submit</Button>
-                        </div>
-                    </Form>
-                </Grid.Column>
-                <Grid.Column width={4}></Grid.Column>
-            </Grid>
-            
-        </Layout>
+    const contract = getServerContract();
+    const grant = GrantOpportunity.parse(await contract.grants(id));
+    grant.agencyName = (await contract.addressToGrantor(grant.agencyAddress)).agencyName;
+
+    return {
+        id,
+        grant
     }
 }
--- pages/projects/new.tsx
+++ pages/projects/new.tsx
@@ -1,44 +1,62 @@
-import React, { Component } from "react";
+import { useRouter } from "next/router";
+import React, { Component, useState } from "react";
 import { Button, Checkbox, Dropdown, Form, Grid, Message, Table } from "semantic-ui-react";
 import Layout from "../../components/Layout";
+import { getRwContract } from "../../ethereum/clientContract";
 
-export default class NewProject extends Component {
-    static async getInitialProps(props: any) {
-        const { id } = props.query;
-        if(typeof id === 'undefined'){
-            return {};
-        }
+export default function NewProject(){
+    const [ agreedToTerms, setAgreedToTerms ] = useState(false);
+    const [ address, setAddress ] = useState('');
+    const [ name, setName ] = useState('');
+    const [ description, setDescription ] = useState('');
 
-        return {
-            id
+    const [ isLoading, setIsLoading ] = useState(false);
+
+    const router = useRouter();
+
+    const handleSubmit = async () => {
+        const contract = await getRwContract();
+        try{
+            setIsLoading(true);
+            await contract.createProject(address, name, description);
+
+            router.push('/user');
+        }catch(e){
+
+        }finally{
+            setIsLoading(false);
         }
     }
 
-    render() {
-        return <Layout>
-            <Grid columns={3}>
-                <Grid.Column width={4}></Grid.Column>
-                <Grid.Column width={8}>
-                    <Form>
-                        <Form.Field>
-                            <label>Project Name</label>
-                            <Form.Input/>
-                        </Form.Field>
-                        <Form.Field>
-                            <label>Project Description</label>
-                            <Form.Input/>
-                        </Form.Field>
-                        <Form.Field>
-                            <Checkbox label='I agree to the Terms and Conditions' />
-                        </Form.Field>
-                        <div style={{ textAlign: 'center' }}>
-                            <Button  type='submit'>Submit</Button>
-                        </div>
-                    </Form>
-                </Grid.Column>
-                <Grid.Column width={4}></Grid.Column>
-            </Grid>
-            
-        </Layout>
-    }
+    const isFormValid = () => agreedToTerms && name.length > 0 && description.length > 0 && address.length > 0;
+
+    return <Layout>
+        <Grid columns={3}>
+            <Grid.Column width={4}></Grid.Column>
+            <Grid.Column width={8}>
+                <Form>
+                    <Form.Field>
+                        <label>Project Address</label>
+                        <Form.Input value={address} onChange={e => setAddress(e.target.value)} />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Project Name</label>
+                        <Form.Input placecholder='Put at least 10 characters' value={name} onChange={e => setName(e.target.value)} />
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Project Description</label>
+                        <Form.Input placecholder='Put at least 100 characters' value={description} onChange={e => setDescription(e.target.value)} />
+                    </Form.Field>
+                    <Form.Field>
+                        <Checkbox checked={agreedToTerms} onChange={() => setAgreedToTerms(!agreedToTerms)} label='I agree to the Terms and Conditions' />
+                    </Form.Field>
+                    <div style={{ textAlign: 'center' }}>
+                        <Button loading={isLoading} disabled={!isFormValid() || isLoading} onClick={() => handleSubmit()} type='submit'>Submit</Button>
+                    </div>
+                </Form>
+            </Grid.Column>
+            <Grid.Column width={4}></Grid.Column>
+        </Grid>
+    </Layout>
 }
+
