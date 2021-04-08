cbdbd1659b52fb06cd5e305649a065368c54d903
Renegade <psyud@pm.me>
2021-04-08 17:03:09 +0700

    UX signup

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Button, Dropdown, Form, Grid, Message } from "semantic-ui-react";
 import Layout from "../components/Layout";
 import RedirectIfLoggedIn from "../components/RedirectIfLoggedIn";
+import TransactionMessages from "../components/TransactionMessages";
 import { getRwContract } from "../ethereum/clientContract";
 import { selectWallet, walletConnected } from "../slices/walletSlice";
 import { getWeb3Provider } from "../utils/clientUtils";
@@ -30,6 +31,8 @@ export default function SignUp() {
     const [ applicantEmailAddress, setApplicantEmailAddress ] = useState('');
     const [ applicantPhysicalAddress, setApplicantPhysicalAddress ] = useState('');
 
+    const [ txHash, setTxHash ] = useState(null);
+
     const isFormValid = () => {
         if(selectedRole == Role.Grantor){
             return agencyName.length > 0 && agencyCode.length > 0;
@@ -47,18 +50,18 @@ export default function SignUp() {
         try {
             setSigningUp(true);
             if(selectedRole === Role.Grantor){
-                await contract.registerAsGrantor(agencyName, agencyCode);
+                const res = await contract.registerAsGrantor(agencyName, agencyCode);
+                setTxHash(res.hash);
             }else if(selectedRole === Role.Applicant){
-                await contract.registerAsApplicant(applicantName, applicantPhoneNumber, applicantEmailAddress, applicantPhysicalAddress);
+                const res = await contract.registerAsApplicant(applicantName, applicantPhoneNumber, applicantEmailAddress, applicantPhysicalAddress);
+                setTxHash(res.hash);
             }
-            router.replace('/');
         }catch(e){
-            setErrorMessage(e.message ?? 'Something wrong happened');
+            setErrorMessage((e.data && e.data.message) ?? 'Something wrong happened');
         }finally{
             setSigningUp(false);
         }
     }
-
 
     const connectWallet = async () => {
         try{
@@ -155,31 +158,34 @@ export default function SignUp() {
 
     return <Layout>
         <RedirectIfLoggedIn>
-            <Grid>
-                <Grid.Column width={3}></Grid.Column>
-                <Grid.Column 
-                    width={10}
-                    style={{ 
-                        padding: '10em'
-                    }}
-                    textAlign='center'
-                >
-                    <Grid.Row>
+         <Grid>
+                    <Grid.Column width={3}></Grid.Column>
+                    <Grid.Column 
+                        width={10}
+                        style={{ 
+                            padding: '10em'
+                        }}
+                        textAlign='center'
+                    >
+                        <Grid.Row>
+                            {
+                                !wallet.isMetaMaskInstalled && <Message warning>Please install MetaMask</Message> 
+                            }
+                            {
+                                !txHash && wallet.isMetaMaskInstalled && renderForm()
+                            }
+                            {
+                                txHash && <TransactionMessages txHashes={[txHash]}/>
+                            }
+                        </Grid.Row>
+                    <Grid.Row style={{ marginTop: '0.5em' }}>
                         {
-                            !wallet.isMetaMaskInstalled && <Message warning>Please install MetaMask</Message> 
-                        }
-                        {
-                            wallet.isMetaMaskInstalled && renderForm()
+                            errorMessage.length > 0 && <Message error>{errorMessage}</Message>
                         }
                     </Grid.Row>
-                   <Grid.Row style={{ marginTop: '0.5em' }}>
-                       {
-                           errorMessage.length > 0 && <Message error>{errorMessage}</Message>
-                       }
-                   </Grid.Row>
-                </Grid.Column>
-                <Grid.Column width={3}></Grid.Column>
-            </Grid>
+                    </Grid.Column>
+                    <Grid.Column width={3}></Grid.Column>
+                </Grid>
         </RedirectIfLoggedIn>
         </Layout>
 }
