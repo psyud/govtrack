5b0188ca127a5832954a22b00326232bc08106ff
Renegade <psyud@pm.me>
2021-04-08 13:31:51 +0700

    set up local pipeline

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 const dotenv = require('dotenv');
 dotenv.config();
-
 module.exports = {
     serverRuntimeConfig: {
-        INFURA_KEY: process.env.INFURA_KEY
+        INFURA_KEY: process.env.INFURA_KEY,
+        LOCAL: process.env.ENV,
+        GANACHE: 'http://0.0.0.0:8545'
     },
     publicRuntimeConfig: {
-        CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS,
-        SUBGRAPH_API: process.env.SUBGRAPH_API
+        CONTRACT_ADDRESS: process.env.ENV === 'local' ? process.env.LOCAL_CONTRACT_ADDRESS : process.env.CONTRACT_ADDRESS,
+        SUBGRAPH_API: process.env.ENV === 'local' ? process.env.LOCAL_SUBGRAPH_API: process.env.SUBGRAPH_API,
     },
   }
